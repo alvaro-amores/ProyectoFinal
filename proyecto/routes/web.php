416e6d9c029087f32111e478cf1a5116eb3257b0
harols10 <harol88888@gmail.com>
2022-05-28 16:29:08 +0200

    quinto

--- proyecto/routes/web.php
+++ proyecto/routes/web.php
@@ -37,3 +37,10 @@ Route::get('/carrito', function () {
 Route::get('/compras', function () {
     return view('compras');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
